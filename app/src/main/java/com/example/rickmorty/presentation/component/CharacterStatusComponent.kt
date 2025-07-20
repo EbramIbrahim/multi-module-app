a7a2d6535fcd70e998b4b8deae1c9a78304414ee
EbramIbrahim <104721363+EbramIbrahim@users.noreply.github.com>
2025-07-20 14:35:51 +0300

    add data point component

--- app/src/main/java/com/example/rickmorty/presentation/component/CharacterStatusComponent.kt
+++ app/src/main/java/com/example/rickmorty/presentation/component/CharacterStatusComponent.kt
@@ -9,8 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.network.models.domain.CharacterStatus
--- app/src/main/java/com/example/rickmorty/presentation/ui/CharacterDetailsScreen.kt
+++ app/src/main/java/com/example/rickmorty/presentation/ui/CharacterDetailsScreen.kt
@@ -1,17 +1,24 @@
 package com.example.rickmorty.presentation.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.network.models.domain.Character
+import com.example.rickmorty.ui.theme.RickAction
+import com.example.rickmorty.ui.theme.RickTextPrimary
 
 @Composable
 fun CharacterDetailsScreen() {
@@ -56,9 +63,34 @@ fun CharacterDetailsScreen() {
 }
 
 
+@Composable
+fun DataPointComponent(dataPoint: DataPoint) {
+
+    Column {
+        Text(
+            text = dataPoint.title,
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = RickAction
+            )
+        )
+
+        Text(
+            text = dataPoint.description,
+            style = TextStyle(
+                fontSize = 24.sp,
+                color = RickTextPrimary
+            )
+        )
+    }
+    
+}
+
+
 data class DataPoint(
     val title: String,
-    val data: String
+    val description: String
 )
 
 
