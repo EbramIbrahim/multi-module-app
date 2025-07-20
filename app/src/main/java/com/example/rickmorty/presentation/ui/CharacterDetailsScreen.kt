ce5e46e767ae673a263518f67ade4b3be7137785
EbramIbrahim <104721363+EbramIbrahim@users.noreply.github.com>
2025-07-20 14:44:00 +0300

    add character details screen component

--- app/src/main/java/com/example/rickmorty/presentation/ui/CharacterDetailsScreen.kt
+++ app/src/main/java/com/example/rickmorty/presentation/ui/CharacterDetailsScreen.kt
@@ -3,8 +3,14 @@ package com.example.rickmorty.presentation.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,11 +18,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.example.network.models.domain.Character
+import com.example.network.models.domain.CharacterStatus
+import com.example.rickmorty.presentation.component.CharacterDetailsNamePlateComponent
 import com.example.rickmorty.ui.theme.RickAction
 import com.example.rickmorty.ui.theme.RickTextPrimary
 
@@ -56,8 +66,29 @@ fun CharacterDetailsScreen() {
                 }
             }
         }
+        item {
+            CharacterDetailsNamePlateComponent(
+                name = "",
+                status = CharacterStatus.Alive
+            )
+        }
+        item { Spacer(modifier = Modifier.height(8.dp)) }
 
+        item {
+            AsyncImage(
+                model = "",
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(12.dp))
+            )
+        }
 
+        items(characterDataPoints) {
+            Spacer(modifier = Modifier.height(32.dp))
+            DataPointComponent(it)
+        }
     }
 
 }
@@ -84,7 +115,7 @@ fun DataPointComponent(dataPoint: DataPoint) {
             )
         )
     }
-    
+
 }
 
 
